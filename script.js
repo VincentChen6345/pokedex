c6d227a11f16257bd97ebce5ac503e051bf480a7
VincentChen6345 <vincent.chen6345@hotmail.com>
2023-03-02 17:03:37 +1100

    pokedex complete

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
 let pokemonList = [];
 let dexNumber;
-const select = document.getElementById("pokemon-list");
+let spriteURL;
+let pkmTypes = [];
 
+const typeColours = {
+  normal: `rgb(158, 158, 109)`,
+  fighting: "rgb(184, 42, 36)",
+  flying: "rgb(158, 133, 238)",
+  poison: `rgb(196, 97, 226)`,
+  ground: "rgb(220, 184, 93)",
+  rock: "rgb(175, 149, 49)",
+  bug: "rgb(158, 175, 30)",
+  ghost: "rgb(101, 78, 141)",
+  steel: "rgb(175, 175, 202)",
+  fire: "rgb(238, 116, 43)",
+  water: "rgb(93, 133, 238)",
+  grass: "rgb(32, 178, 44)",
+  electric: " rgb(247, 201, 43)",
+  psychic: "rgb(247, 77, 125)",
+  ice: "rgb(141, 211, 211)",
+  dragon: "rgb(100, 50, 247)",
+  fairy: "rgb(238,173,180)",
+};
+let updatedStats = {};
+/////////////////////
+const select = document.getElementById("pokemon-list");
+const spriteImg = document.getElementById("sprite-image");
+const pkmName = document.getElementById("name");
+const typeContainer = document.getElementById("type-container");
+const dexEntry = document.getElementById("dex-entry");
+const hp = document.getElementById("HP");
+const atk = document.getElementById("ATK");
+const def = document.getElementById("DEF");
+const spATK = document.getElementById("SP-ATK");
+const spDEF = document.getElementById("SP-DEF");
+const spe = document.getElementById("SPE");
+const mainContainer = document.getElementById("main-container");
+const nextBTN = document.getElementById("next");
+const previousBTN = document.getElementById("previous");
+
+/////////////////////
+//create new option element
 const createOptionEl = (name, number) => {
-  //create new option element
   const optionEl = document.createElement("option");
   optionEl.value = `${name}`;
   optionEl.textContent = `${number}. ${name}`;
-  optionEl.number = number;
+  optionEl.setAttribute("number", number);
   select.appendChild(optionEl);
 };
-//loop through pokemon list array
+//loop through pokemon list array to create option elements for each pokemon
 const updateSelect = () => {
   pokemonList.forEach((element, index) => {
     const pkmNameLower = element.name;
@@ -27,10 +65,135 @@ const getPokemonList = async () => {
   pokemonList = await fetch(apiURL + `?offset=0&limit=151`)
     .then((res) => res.json())
     .then((res) => res.results);
-  //
-  console.log(pokemonList);
+
   //await fetch(`url`).json() returns a promise. You must use a promise method to handle promises i.e. then((res)=>res.json())
   updateSelect();
   return pokemonList;
 };
 getPokemonList();
+
+//dynamically set the src attribute for the sprite image
+const updateSpriteImg = (number) => {
+  spriteImg.src = `
+  https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${number}.png
+  `;
+};
+
+//update pokemon name
+const updatePkmName = async (value) => {
+  dexNumber = select.selectedOptions[0].getAttribute("number");
+  //.selectedOptions property is a read-only collection that contains all the 'option' elements in a select element. select.selectedOptions[0] is used to retrieve the currently selected 'options' element. '[0]' is used to access the first element in the selectedOptions collection which is the only element when 'select' element has a single collection
+  let name = `#${dexNumber} ${value}`;
+  pkmName.textContent = name;
+  if (name.length > 12) {
+    pkmName.style.fontSize = `35px`;
+  } else {
+    pkmName.style.fontSize = `40px`;
+  }
+};
+//update background color to a lighter version of it's type color
+const updateBGColor = () => {
+  let bgColor = typeColours[pkmTypes[0]];
+  let fadedBgColor = bgColor.slice(0, -1) + `, 0.6)`;
+  mainContainer.style.backgroundColor = fadedBgColor;
+};
+//update pokemon type
+const getPkmType = async (number) => {
+  pkmTypes = [];
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`).then(
+    (res) => res.json()
+  );
+  const types = data.types;
+  types.forEach((object) => {
+    pkmTypes.push(object.type.name);
+  });
+
+  updateTypeUI();
+  updateBGColor();
+};
+
+//create pokemon type bubbles
+const updateTypeUI = () => {
+  typeContainer.innerHTML = ``;
+  pkmTypes.forEach((value) => {
+    const typeEl = document.createElement("span");
+    typeEl.classList.add(`type`);
+    // typeEl.classList.add(`${value}`);
+
+    typeEl.style.backgroundColor = `${typeColours[value]}`;
+    typeEl.textContent = value;
+
+    typeContainer.appendChild(typeEl);
+  });
+};
+
+//get pokemon dex entry
+const getDexEntry = async (number) => {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${number}/`
+  ).then((res) => res.json());
+  const entry = data["flavor_text_entries"][10]["flavor_text"];
+  dexEntry.textContent = entry;
+};
+//update stats UI
+const updateStatsUI = () => {
+  hp.textContent = `HP: ${updatedStats.hp}`;
+  atk.textContent = `ATK: ${updatedStats.attack}`;
+  def.textContent = `DEF: ${updatedStats.defense}`;
+  spATK.textContent = `SP-ATK: ${updatedStats["special-attack"]}`;
+  spDEF.textContent = `SP-DEF: ${updatedStats["special-defense"]}`;
+  spe.textContent = `SPE: ${updatedStats["speed"]}`;
+};
+//get base stats
+const getStats = async (number) => {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`).then(
+    (res) => res.json()
+  );
+  const statsArray = data.stats;
+  statsArray.forEach((stat) => {
+    updatedStats[stat.stat.name] = stat["base_stat"];
+  });
+  updateStatsUI();
+};
+
+// select onChange functionality to trigger UI updates
+const onSelectChange = () => {
+  updatePkmName(select.value);
+  updateSpriteImg(dexNumber);
+  getPkmType(dexNumber);
+  getDexEntry(dexNumber);
+  getStats(dexNumber);
+};
+select.addEventListener("change", onSelectChange);
+
+// button functionality
+const prevOption = () => {
+  //get current selected index
+  const currentIndex = select.selectedIndex;
+  const prevIndex = currentIndex - 1;
+  if (prevIndex === -1) {
+    select.selectedIndex = 150;
+
+    onSelectChange();
+  } else {
+    select.selectedIndex = prevIndex;
+    onSelectChange();
+  }
+};
+
+const nextOption = () => {
+  //get current selected index
+  const currentIndex = select.selectedIndex;
+  const nextIndex = currentIndex + 1;
+  if (nextIndex === 151) {
+    select.selectedIndex = 0;
+
+    onSelectChange();
+  } else {
+    select.selectedIndex = nextIndex;
+    onSelectChange();
+  }
+};
+// nextBTN.addEventListener("click", ());
+previousBTN.addEventListener("click", prevOption);
+nextBTN.addEventListener("click", nextOption);
